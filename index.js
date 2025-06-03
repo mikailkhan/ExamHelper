8ecddcd09761c73e7164c9114d593bbe875722a4
Mikail Khan <mikailkhan@192.168.1.11>
2025-06-03 18:58:09 +0500

    add review functionality in index.js and review.ejs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Imports
 import express from 'express';
 import bodyParser from 'body-parser';
 import OpenAI from "openai";
@@ -6,23 +7,28 @@ import { zodTextFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
 
-
+// System Settings
 const PORT = 3000;
 const app = express();
 
-
 dotenv.config();
 
-let totalQuestions = 0;
-let examCompleted = false;
+app.use(bodyParser.urlencoded());
+app.use(express.static("public"));
 
-let structuredMCQS;
-
+// Open AI connection
 const client = new OpenAI({
     apiKey: process.env['API_KEY'], // This is the default and can be omitted
 });
 
-const MCQ = z.object ({
+// Exam Variables
+let totalQuestions = 0;
+let examCompleted = false;
+let structuredQuestions;
+
+
+// Setting Structure for Questions based on exam type
+const MCQExamStructure = z.object ({
     question: z.string(),
     a: z.string(),
     b: z.string(),
@@ -33,16 +39,30 @@ const MCQ = z.object ({
 });
 
 const MCQList = z.object({
-    mcqs:  z.array(MCQ)
+    mcqs:  z.array(MCQExamStructure)
 });
 
-app.use(bodyParser.urlencoded());
-app.use(express.static("public"));
+const ReviewExamStructure = z.object ({
+    question: z.string(),
+    marks: z.string(),
+    markingScheme: z.string()
+});
 
+const ReviewQuestionList = z.object({
+    exam: z.array(ReviewExamStructure)
+});
+
+
+// Handling Requests 
+
+
+// Home Page
 app.get("/", (req, res)=>{
     res.render("index.ejs");
 });
 
+
+// When user submits request on home page for exam
 app.post("/start", async (req, res) => {
     
     const subject = req.body.subject;
@@ -52,16 +72,20 @@ app.post("/start", async (req, res) => {
     totalQuestions = req.body.totalQuestions;
     let prompt = `{Subject: ${subject}}, \n {Topic: ${topic}}, \n {Topic Content: ${content}}`;
     let instructions = "";
+    let structure = "";
+    let structureObject = "";
 
     if (type == "quiz") {
         // quiz 
          instructions = `Generate a MCQ-based quiz with ${totalQuestions} questions from the information in the following format: Question A B C D Answer Explaination`; 
+         structure = MCQList;
+         structureObject = "mcqObject";
     } else {
         // Review Questions
-
+        instructions = `Generate a review based exam with ${totalQuestions} questions from the information in the following format: Question Marks MarkingScheme`;
+        structure = ReviewQuestionList;
+        structureObject = "reviewExamObject";
     }
-
-    
 
     const response = await client.responses.parse({
     model: 'gpt-4o',
@@ -76,26 +100,35 @@ app.post("/start", async (req, res) => {
     }
     ],
     text: {
-        format: zodTextFormat(MCQList, "mcqObject")
+        format: zodTextFormat(structure, structureObject)
     }
     });
 
-    structuredMCQS = response.output_parsed;
+    structuredQuestions = response.output_parsed;
 
-    res.redirect(303, "/quiz");    
+    if (type == "quiz") {
+        res.redirect(303, "/quiz");    
+    } else {
+        res.redirect(302, "/review");
+    }
+    
 });
 
+
+// Quiz start page
 app.get("/quiz",(req, res)=>{
     let quizData = {
-        quiz: structuredMCQS
+        quiz: structuredQuestions
     }
-    if (structuredMCQS) {
+    if (structuredQuestions) {
         res.render("quiz.ejs", quizData);
     } else {
         res.redirect(302, "/");
     }
 });
 
+
+// After quiz is submitted 
 app.post("/quizresult", (req, res) => {
     const answerMCQs = req.body;
 
@@ -106,7 +139,7 @@ app.post("/quizresult", (req, res) => {
     for (const key in answerMCQs) {        
         // Removes the mcq part from the name to return the number of the mcq
         let answeredMCQNumber = parseInt(key.slice(3)) - 1; 
-        let chosenMCQ = structuredMCQS.mcqs[answeredMCQNumber];
+        let chosenMCQ = structuredQuestions.mcqs[answeredMCQNumber];
 
         if (chosenMCQ.answer.toLowerCase() == answerMCQs[key].toLowerCase()) {
             chosenMCQ.filled = 1;
@@ -130,7 +163,7 @@ app.post("/quizresult", (req, res) => {
         correctMCQs:  correctMCQS,
         wrongMCQS: wrongMCQS,
         leftMCQS: leftMCQS,
-        quiz: structuredMCQS
+        quiz: structuredQuestions
     }
 
     console.log(quizData);
@@ -138,11 +171,87 @@ app.post("/quizresult", (req, res) => {
     res.render("quiz.ejs", quizData);
 });
 
+
+// Review Exam Page
 app.get("/review",(req, res)=>{
-    res.render("review.ejs");
+    let reviewExamData = {
+        review: structuredQuestions
+    };
+    res.render("review.ejs", reviewExamData);
 });
 
+// Result
+app.post("/reviewresult", async (req, res) => {
+    const userResponse = req.body;
 
+    let totalMarks = 0;
+    let totalObtainedMarks = 0;
+    
+    for (const key in userResponse) {
+        structuredQuestions.exam[parseInt(key.slice(6)) - 1].userAnswer = userResponse[key];  
+        structuredQuestions.exam[parseInt(key.slice(6)) - 1].userAnswerKey = key;
+    }
+
+    let instructions = "Check the user answer against the question and its marking scheme. Provide the userAnswerKey, obtained marks, and feedback";
+    let prompt = "";
+    
+    structuredQuestions.exam.forEach(question => {
+        prompt += JSON.stringify(question);
+        totalMarks += parseInt(question.marks);  
+    });
+
+    // Setting Structure for Questions based on exam type
+    const feedbackStructure = z.object ({
+        userAnswerKey: z.string(),
+        obtainedMarks: z.string(), 
+        feedback: z.string(),
+    });
+
+    const feedbackList = z.object({
+        feedback:  z.array(feedbackStructure)
+    });
+
+    const response = await client.responses.parse({
+    model: 'gpt-4o',
+    input: [
+    { 
+        role: "system",
+        content: instructions
+    },
+    {
+        role: "user",
+        content: prompt
+    }
+    ],
+    text: {
+        format: zodTextFormat(feedbackList, "feedback")
+    }
+    });
+
+    let checkedResponse = response.output_parsed;
+
+    // console.log(checkedResponse);
+
+
+    checkedResponse.feedback.forEach(eachFeedback => {
+        totalObtainedMarks += eachFeedback.obtainedMarks;
+        let key = parseInt(eachFeedback.userAnswerKey.slice(6)) - 1;
+        structuredQuestions.exam[key].obtainedMarks = eachFeedback.obtainedMarks;
+        structuredQuestions.exam[key].feedback = eachFeedback.feedback;
+    });
+
+    let reviewExamData = {
+        examCompleted: true,
+        review: structuredQuestions,
+        totalObtainedMarks: totalObtainedMarks
+    };
+
+
+    res.render("review.ejs", reviewExamData);
+    console.log(reviewExamData);
+});
+
+// Server Start
 app.listen(PORT, ()=>{
     console.log(`Server successfully started on port ${PORT}`);
 });
